f062f5baa796f80a2e23ad7e1e8aa4b4703d8ad2
chinwendueke <ekealexandra5@gmail.com>
2022-10-29 03:38:37 +0000

    syntax

--- try.sh
+++ try.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-cat /etc/*release | head -4 |tail -1 | awk -F= '{print$2}'
-op=`cat /etc/*release | head -4 |tail -1 | awk -F= '{print$2}'`
-if [ $op == centos ];
-then 
-sudo yum install finger
+#
+OPERS=$(cat /etc/*release | head -4 |tail -1 | awk -F= '{print$2}' | sed 's/"//g')
+UB=(Ubuntu)
+RHEL='(CentOS.*)|(Fedora)|(Red Hat.*)'
+ALP=(Alpine)
+ORC=(Oracle)
+DEB=(Debian)
+
+
+if [ $OPERS = $RHEL ]; then
+ 
+echo "OS: $OPERS"
+echo "You got it"
+#
+sudo yum install finger -y
+
+elif [ $OPERS = $UB ]; then
+ 
+echo "OS: $OPERS"
+sudo apt install unzip -y
 else 
 echo "not working" 
 fi
